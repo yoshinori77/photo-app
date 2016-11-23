407fca8e4542eeb0bdc7f7e3b1f0e3b4ae6cd266
yamashitayoshinori <yoshinoriyamashita123@gmail.com>
2016-11-23 12:58:00 +0000

    Add signup view and styling

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,7 +8,8 @@ Rails.application.configure do
 
   # Do not eager load code on boot.
   config.eager_load = false
-
+  config.action_mailer.delivery_method = :test
+  config.action_mailer.default_url_options = { host: 'http://newcompleterailscourse-yoshinori77.c9users.io:8080' }
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
